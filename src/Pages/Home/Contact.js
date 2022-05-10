e468d89235581e2e1fd108829de81549af736b37
Shahidul Islam <shahidulislam7150@gmail.com>
2022-05-10 22:46:38 +0600

    contact form response

--- src/Pages/Home/Contact.js
+++ src/Pages/Home/Contact.js
@@ -9,42 +9,25 @@ const Contact = () => {
           className="p-12">
           <h3 className='text-primary text-center font-bold'>Contact Us</h3>
           <h2 className='text-3xl text-white text-center mb-5'>Stay connect with us</h2>
-      <form className='w-[50%] mx-auto'>
+      <form className='w-[100%] lg:w-[30%] mx-auto'>
         <div class="mb-6">
-          <label
-            for="email"
-            class="block mb-2 text-sm font-medium text-white "
-          >
-            Your email
-          </label>
           <input
             type="email"
             id="email"
             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-            placeholder="name@doctorlike.com"
+            placeholder="Email Address"
             required
           ></input>
         </div>
         <div class="mb-6">
-          <label
-            for="base-input"
-            class="block mb-2 text-sm font-medium text-white "
-          >
-            Base input
-          </label>
           <input
             type="text"
             id="base-input"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                      placeholder="Subject"
           ></input>
         </div>
         <div class="mb-6">
-          <label
-            for="message"
-            class="block mb-2 text-sm font-medium text-white "
-          >
-            Your message
-          </label>
           <textarea
             id="message"
             rows="4"
